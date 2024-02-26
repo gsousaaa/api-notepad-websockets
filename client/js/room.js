deea29ca3e28c6196643f68773c09232679a135b
gsousaaa <glaucoezequiel94@gmail.com>
2024-02-26 10:34:55 -0300

    Correção para a persistencia do conteúdo da nota ao recarregar a página

--- client/js/room.js
+++ client/js/room.js
@@ -32,15 +32,15 @@ roomContentTextArea.addEventListener("keyup", async (e)=> {
 })
 
 window.addEventListener('load', async ()=> {
-  const data = await fetch(`http://localhost:3000/api/get-notepad/${roomName}`)
+  const data = await fetch(`http://localhost:3000/api/get-notepad/${roomName}`).then(res => res.json())
+  
+  roomContentTextArea.value = await data.noteContent
 })
-
 
 if(roomName) {
     const channel = pusher.subscribe(roomName);
+   
     channel.bind("updated-note", data => {
-        console.log( { data } )
-        console.log(data.userID)
         // Só atualizar a tela se o UserID for diferente do atual, resolvendo o problema de flics na tela.
         if(data.content && data.userID != pusher.sessionID) {
             roomContentTextArea.value = data.content
